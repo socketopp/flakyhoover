fac9c64a5adbf83dda785a47d50ef5c0149cd663
andla830 <andla830@student.liu.se>
2019-07-11 16:40:19 +0200

    added documentreader

---
+++ flakyhoover/src/main/java/util/FlakyFile.java
@@ -0,0 +1,130 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import flakyhoover.AbstractFlaky;
+
+public class FlakyFile {
+
+	public FlakyFile(int id, String url, String sha, int testCount, String module, String testName, String category,
+			String version, String className, String testMethod) {
+		super();
+		this.className = className;
+		this.testMethod = testMethod;
+		this.id = id;
+		this.url = url;
+		this.sha = sha;
+		this.testCount = testCount;
+		this.module = module;
+		this.testName = testName;
+		this.category = category;
+		this.version = version;
+		this.setSmellyInstances(new ArrayList<AbstractFlaky>());
+	}
+
+	private List<AbstractFlaky> smellyInstances;
+	private int id;
+	private String className;
+	private String testMethod;
+	private String url;
+	private String sha;
+	private int testCount;
+	private String module;
+	private String testName;
+	private String category;
+	private String version;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getSha() {
+		return sha;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setSha(String sha) {
+		this.sha = sha;
+	}
+
+	public int getTestCount() {
+		return testCount;
+	}
+
+	public void setTestCount(int testCount) {
+		this.testCount = testCount;
+	}
+
+	public String getModule() {
+		return module;
+	}
+
+	public void setModule(String module) {
+		this.module = module;
+	}
+
+	public String getTestName() {
+		return testName;
+	}
+
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getTestMethod() {
+		return testMethod;
+	}
+
+	public void setTestMethod(String testMethod) {
+		this.testMethod = testMethod;
+	}
+
+	public List<AbstractFlaky> getSmellyInstances() {
+		return smellyInstances;
+	}
+
+	public void setSmellyInstances(List<AbstractFlaky> smellyInstances) {
+		this.smellyInstances = smellyInstances;
+	}
+
+	public void addSmellyInstances(AbstractFlaky flaky) {
+		smellyInstances.add(flaky);
+	}
+
+}
